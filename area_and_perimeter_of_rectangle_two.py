d42dd8cc245469cae8bef7026cd70677788cdbdc
Ubuntu <ubuntu@ip-172-31-76-11.ec2.internal>
2020-11-24 21:28:00 +0000

    This program calculates area and perimeter of a rectangle

---
+++ area_and_perimeter_of_rectangle_two.py
@@ -0,0 +1,18 @@
+# 1/usr/bin/env python3
+
+# Created by Larry Nkengbeza
+# Created on NOvember 2020
+# This program is meant to calculate area and perimeter of a circel
+# with a radius of 15mm
+
+def main():
+    print("Area is {}cm^2".format(7*1))
+    print("perimeter is{}cm^2".format(2*(2+1)))
+    print("Area is {}cm^2".format(4*5))
+    print("Perimeter is {}cm^2".format(2*(7+8)))
+    print("Area is {}cm^2".format(8*2))
+    print("Perimeter is {}cm^2".format(2*(2+9)))
+
+
+if __name__ == "__main__":
+    main()
